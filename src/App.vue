210467108f84d62b1d979c83ef094f0be22d5350
ilogic-3-a <avasad913@gmail.com>
2025-04-17 20:05:59 +0500

    hhhdh

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <router-view />
+  <div id="app">
+    <FabricCanvas />
+  </div>
+<h3>dsa</h3>
 </template>
-
-<script setup>
-//
+<script>
+import FabricCanvas from './components/FabricCanvas.vue';
+export default {
+  name: 'App',
+  components: {
+    FabricCanvas,
+  },
+};
 </script>
---
+++ src/components/FabricCanvas.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <canvas ref="canvas" width="400" height="400"></canvas>
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { Canvas, Rect, Circle, Text, Polygon } from 'fabric';
+
+const canvas = ref();
+onMounted(() => {
+  const fabricCanvas = new Canvas(canvas.value);
+  const rect = new Rect({
+    left: 100,
+    top: 100,
+    fill: 'blue',
+    width: 100,
+    height: 100,
+  });
+  const circle = new Circle({
+    top: 100,
+    left: 300,
+    width: 100,
+    height: 100,
+    fill: 'yellow',
+    radius: 20,
+  });
+
+  // const polygon = new Polygon({
+  //   top: 100,
+  //   left: 100,
+  //   width: 100,
+  //   height: 100,
+  //   fill: 'green',
+  // });
+  fabricCanvas.add(rect, circle);
+  // fabricCanvas.add(polygon);
+
+
+});
+</script>
+<style scoped>
+canvas {
+  border: 1px solid #ccc;
+}
+</style>
