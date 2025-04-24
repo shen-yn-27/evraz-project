e2d5191dda8a96789366f0be1beead1bb87044ca
shen-yn-27 <shen.yn.27@gmail.com>
2025-04-24 20:00:22 +0500

    вавапоаа

--- src/components/FabricCanvas.vue
+++ src/components/FabricCanvas.vue
@@ -15,12 +15,13 @@
     <div class="USSR">
       <h9>Кухня</h9>
       <p>Температура: </p>
-      <span>{{sliderValue}}°C</span>
+
+      <span>{{sliderValue - 5 }}°C</span>
     </div>
     <div class="France">
       <h9>Ванная</h9>
       <p>Температура: </p>
-      <span>{{sliderValue}}°C</span>
+      <span>{{sliderValue - 5}}°C</span>
     </div>
     <div class="China">
       <h9>Обратный коллектор</h9>
@@ -29,40 +30,51 @@
     </div>
   </div>
   <div>
-    <input
-    type="range"
-    min="0"
-    max="100"
-    value="40"
-    class="slider"
-    v-model="sliderValue">
-  <input v-model="sliderValue" type="number">
-    <br/>
 
-  <input
-    type="range"
-    min="0"
-    max="100"
-    value="40"
-    class="slider1"
-    v-model="sliderValue1">
-  <input v-model="sliderValue1" type="number">
-
-    <br/>
-  <input
-    type="range"
-    min="0"
-    max="100"
-    value="40"
-    class="slider2"
-    v-model="sliderValue2">
-  <input v-model="sliderValue2" type="number">
-    <br/>
   </div>
   <div class="Japan">
-    <p>Температура котла(°C): </p>
-    <p>Целевая температура кухни(°C): </p>
-    <p>Целевая температура ванной(°C): </p>
+    <div class="row items-center q-gutter-sm">
+      <div>Температура котла(°C): </div>
+      <input
+        type="range"
+        min="40"
+        max="80"
+        value="40"
+        class="slider"
+        v-model="sliderValue"
+      >
+      <input v-model="sliderValue" type="number">
+    </div>
+
+    <div class="row items-center q-gutter-sm">
+      <div>Целевая температура кухни(°C): </div>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value="40"
+        step="33"
+        class="slider1"
+        v-model="sliderValue1">
+      <input v-model="sliderValue1" type="number">
+    </div>
+
+    <div class="row items-center q-gutter-sm">
+      <div>Целевая температура ванной(°C): </div>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value="40"
+        step="33"
+        class="slider2"
+        v-model="sliderValue2">
+      <input v-model="sliderValue2" type="number">
+    </div>
+
+
+
+
   </div>
 </template>
 <script setup>
@@ -71,9 +83,9 @@ import { ref, onMounted } from 'vue';
 
 const canvas = ref();
 
-const sliderValue = ref(50);
-const sliderValue1 = ref(50);
-const sliderValue2 = ref(50);
+const sliderValue = ref(40);
+const sliderValue1 = ref(0);
+const sliderValue2 = ref(0);
 
 
 
@@ -93,14 +105,14 @@ canvas {
   left: 500px;
   top: 50px;
   width: 100px;
-  height: 100px;
+  height: 150px;
   background-color: indianred;
   border: 2px solid black;
 }
 .Germany{
   position: absolute;
   left: 500px;
-  top: 170px;
+  top: 210px;
   width: 100px;
   height: 150px;
   background-color: lightgreen;
@@ -109,27 +121,27 @@ canvas {
 .USSR{
   position: absolute;
   left: 350px;
-  top: 350px;
+  top: 380px;
   width: 100px;
-  height: 100px;
+  height: 150px;
   background-color: cornflowerblue;
   border: 2px solid black;
 }
 .France{
   position: absolute;
   left: 650px;
-  top: 350px;
+  top: 380px;
   width: 100px;
-  height: 100px;
+  height: 150px;
   background-color: cornflowerblue;
   border: 2px solid black;
 }
 .China{
   position: absolute;
   left: 500px;
-  top: 470px;
+  top: 550px;
   width: 100px;
-  height: 100px;
+  height: 150px;
   background-color: lightgoldenrodyellow;
   border: 2px solid black;
 }
